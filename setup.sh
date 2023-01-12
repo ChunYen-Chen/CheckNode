7c0d23c56cc9c3bdf9348956565af8b0a04fdfd7
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-01-12 21:34:37 +0800

    Separate the `source` part to another step of setup.

--- setup.sh
+++ setup.sh
@@ -25,15 +25,14 @@ else
 fi
 
 # 2. add alias in .bashrc
-printf "Would you like to add an alias to your "$GREEN".bashrc"$WHITE"?[y/n] "
+USER=`whoami`
+BASH_FILE="/home/${USER}/.bashrc"
+printf "Would you like to add an alias to your "$GREEN"${BASH_FILE}"$WHITE"?[y/n] "
 read USE_ALIAS
 
 if [[ "$USE_ALIAS" = "y" || "$USE_ALIAS" = "Y" ]] ; then
     COMMAND="alias node='sh ${WORK_DIR}/check_node.sh'"
-    
-    USER=`whoami`
-    BASH_FILE="/home/${USER}/.bashrc"
-    printf "We will add an alias at the bottom of the "$GREEN"${BASH_FILE}"$WHITE"\n"
+    printf "We will add an alias "$CYAN"node"$WHITE" at the bottom of the "$GREEN"${BASH_FILE}"$WHITE"\n"
 fi
 
 
@@ -49,22 +48,27 @@ read CHECK
 
 if [[ "$CHECK" = "y" || "$CHECK" = "Y" ]] ; then
     echo "directory    ${WORK_DIR}/" > ${WORK_DIR}/config
-    echo "#====================================================" >> ${BASH_FILE}
-    echo "# This the command for checking the node information." >> ${BASH_FILE}
-    echo $COMMAND >> ${BASH_FILE}
-    echo "#====================================================" >> ${BASH_FILE}
-    source ${BASH_FILE}
+    if [[ "$USE_ALIAS" = "y" || "$USE_ALIAS" = "Y" ]] ; then
+        echo "#====================================================" >> ${BASH_FILE}
+        echo "# This the command for checking the node information." >> ${BASH_FILE}
+        echo $COMMAND >> ${BASH_FILE}
+        echo "#====================================================" >> ${BASH_FILE}
+        printf $RED"IMPORTANT!!! "$WHITE
+        printf "Please run the command '"$CYAN"source "${BASH_FILE}$WHITE"' to complete the whole setup!\n"
+    fi
 else
     printf $RED"The setup is not complete!\n"$WHITE
     exit
 fi
 
 # 4. output the tutorial
-echo "Thanks for using the checking node funcion! Here are some examples for you."
-echo "1. List the nodes name, properties, and status."
-echo "node"
-echo "2. List with option."
-echo "node f"
-echo "3. List with multiple options. (The option order does not matter.)"
-echo "node ij"
-echo "To view more option, please read README."
+printf "=============================================================================\n"
+printf "Thanks for using the checking node funcion! Here are some examples for you.\n"
+printf "Note: The command "$CYAN"node"$WHITE" is an alias from "$CYAN"sh "${WORK_DIR}"/check_node.sh"$WHITE".\n"
+printf "1. To list the nodes name, properties, and status: "
+printf $CYAN"node\n"$WHITE
+printf "2. To list with option: "
+printf $CYAN"node f\n"$WHITE
+printf "3. To list with multiple options. (The option order does not matter): "
+printf $CYAN"node ij\n"$WHITE
+printf "To view more option, please read README.md\n"
