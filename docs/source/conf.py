735a28ead36cb99756d6a044ebb950471058af34
Chun-Yen Chen <70311975+ChunYen-Chen@users.noreply.github.com>
2024-04-29 16:29:59 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,10 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'myst_parser',
+    "sphinx_inline_tabs",
+    "sphinx_copybutton",
+    "sphinx_design"
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
