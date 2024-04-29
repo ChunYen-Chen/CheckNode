ac32bdd0d8fb105263ec7dac0cbd499d373b4ddf
Chun-Yen Chen <70311975+ChunYen-Chen@users.noreply.github.com>
2024-04-29 16:42:52 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,13 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# -- Options for MyST --------------------------------------------------------
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist"
+]
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
